580c428960bcec7e0be9660db5b02e1434e85618
lucasbor <lucas.borgne@gmail.com>
2019-12-28 12:27:27 +0100

    2 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,13 @@ import { Component } from '@angular/core';
 })
 export class AppComponent  {
   name = 'Angular';
+  public keysExpression : string = "";
+
+  onKeyPress(value:string){
+    if(!value){
+      return;
+    }
+    this.keysExpression += value + "-";
+  }
+
 }
